f1d69a356ae60b8de714d5a4e11cfce9b5d81203
yes <50946380+yesantikiss@users.noreply.github.com>
2024-09-12 03:58:51 +0000

    cookie

--- stamprally/load_data.js
+++ stamprally/load_data.js
@@ -1,8 +1,21 @@
 const data = [
     {
-        "code" : "TEST"
+        "code" : "TEST",
+        "content" : "<h1>これはテストです</h1>"
     },
     {
-        "code" : "AAAA"
+        "code" : "AAAA",
+        "content" : '<a href="index.html">わぁ</a>'
     }
 ]
+
+function getCookie(name) { //特定のクッキーの取得
+    let cookieArr = document.cookie.split(";");
+    for (let i = 0; i < cookieArr.length; i++) {
+        let cookiePair = cookieArr[i].split("=");
+        if (name === cookiePair[0].trim()) {
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    return null;
+}
--- stamprally/script.js
+++ stamprally/script.js
@@ -5,12 +5,20 @@ const id_result = document.getElementById("result");
 // URLSearchParamsオブジェクトを取得
 let params = url.searchParams;
 let code = params.get("code"); //コードを取得
+let is_stamp = params.get("stamp"); //スタンプを押すか否か
 let check = false; //コードの存在チェック
 
 window.addEventListener("load",function() {//ページが読み込まれた時に実行
   for(let i = 0;i < Object.keys(data).length;i++){
-    if(code === data[i]["code"]) check = true;
+    if(code === data[i]["code"]) {
+      check = true;
+      if(!getCookie(data[i]["code"] && is_stamp === "yes")) {
+        this.document.cookie = data[i]["code"] + "=true";
+      }
+      id_result.innerHTML = data[i]["content"];
+    }
   }
+
   if(check === false){
     this.alert("そんなコードねぇよ");
     window.location.replace("index.html");
