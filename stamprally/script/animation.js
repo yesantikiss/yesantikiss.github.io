c9ce1a39773ed82989f32e4dc7165680a099b16d
yes <50946380+yesantikiss@users.noreply.github.com>
2024-09-19 14:49:26 +0000

    Create a menu

---
+++ stamprally/script/animation.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".menu").forEach(function (el) {
+    const summary = el.querySelector(".icon");
+    const answer = el.querySelector(".list");
+    summary.addEventListener("click", (event) => {
+      // デフォルトの挙動を無効化
+      event.preventDefault();
+      // detailsのopen属性を判定
+      if (el.getAttribute("open") !== null) {
+        // アコーディオンを閉じるときの処理
+        const closingAnim = answer.animate(closingAnimation(answer), animTiming);
+
+        closingAnim.onfinish = () => {
+          // アニメーションの完了後にopen属性を取り除く
+          el.removeAttribute("open");
+        };
+      } else {
+        // open属性を付与
+        el.setAttribute("open", "true");
+        // アコーディオンを開くときの処理
+        const openingAnim = answer.animate(openingAnimation(answer), animTiming);
+      }
+    });
+  });
+});
+
+// アニメーションの時間とイージング
+const animTiming = {
+  duration: 300,
+  easing: "ease",
+};
+
+// アコーディオンを閉じるときのキーフレーム
+const closingAnimation = (answer) => [
+  {
+    width: answer.offsetWidth + "px",
+    opacity: 1,
+  },
+  {
+    width: 0,
+    opacity: 0,
+  },
+];
+
+// アコーディオンを開くときのキーフレーム
+const openingAnimation = (answer) => [
+  {
+    width: 0,
+    opacity: 0,
+  },
+  {
+    width: answer.offsetWidth + "px",
+    opacity: 1,
+  },
+];
